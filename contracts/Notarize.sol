0f611aac1f0f0d0d8b840a76757baf96cb2e7eb8
Maxi Dev <maxi.canellas@gmail.com>
2017-12-13 00:50:08 -0300

    0.1f

--- contracts/Notarize.sol
+++ contracts/Notarize.sol
@@ -1,30 +1,36 @@
 /*
- Bitsign generic notarizacion contract v0.1e
+ Bitsign generic notarizacion contract v0.1f
 */
 pragma solidity ^0.4.15;
 
 contract Notarize {
-  
+
   //State variables
   mapping (bytes32 => address) private proofs;
   address private owner;
-  
-  event Notary(bytes32 _sghash, address _address);
+  address private BSG;
+
+  event Notary(bytes32 _data, address _address);
 
   /*
     Contract constructor takes _user as client Ethereum address
    */
-  function Notarize(address _user) {
+  function Notarize(address _user) public {
+    BSG = msg.sender;
     owner = _user;
   }
 
   /* @params
-    _data signed hash 
+    _data data to sign
    */
-  function notarize(bytes32 _data) {
-    if (msg.sender != owner)
+  function notarize(bytes32 _data) external {
+    if (msg.sender != owner && msg.sender != BSG)
       revert();
     proofs[_data] = msg.sender;
     Notary(_data, msg.sender);
+  }
+
+  function getProof(bytes32 _data) constant public returns (address) {
+    return proofs[_data];
   }
 }
