97cf2ced83542f255878e2ee5d493fa23c5f91be
af <agustinfiorenza@gmail.com>
2018-03-13 09:46:10 -0300

    Minor change

--- contracts/BSGTokenCrowdsale.sol
+++ contracts/BSGTokenCrowdsale.sol
@@ -9,5 +9,7 @@ contract BSGTokenCrowdsale is Crowdsale, WhitelistedCrowdsale {
   function BSGTokenCrowdsale(uint256 _rate, address _wallet, StandardToken _token) public 
     Crowdsale(_rate, _wallet, _token)
     WhitelistedCrowdsale()
-    {}
+    {
+      // Here could go further initialize validations
+    }
 }
